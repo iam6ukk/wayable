012f5a7bc63528ed14f9fd8f73dafcef6255079c
iam6ukk <iann6ukk@gmail.com>
2026-06-21 15:04:44 +0900

    feat: 카카오 로그인 구현

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,3 @@
+class AppConstants {
+  static const kakaoNativeAppKey = 'fc1503cec33269bced0d45bad8b60770';
+}
---
+++ lib/model/user.dart
@@ -0,0 +1,6 @@
+class AppUser {
+  final String uid;
+  final String? nickname;
+
+  AppUser({required this.uid, this.nickname});
+}
